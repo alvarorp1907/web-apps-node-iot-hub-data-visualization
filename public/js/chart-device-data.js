6e28337db1d338bbe7bc630f332d9dd4361e1f21
alvarorp1907 <alvaro.rodriguez.pineiro@alumnos.upm.es>
2025-05-10 17:51:21 +0200

    se han añadido 6 variables mas al grafico de sensores reales

--- public/js/chart-device-data.js
+++ public/js/chart-device-data.js
@@ -15,23 +15,23 @@ $(document).ready(() => {
 	  this.isSimulated = isSimulated;
 	  
 	  if (isSimulated){
-		  console.log("Constructor sensor simulado");
+		  //console.log("Constructor sensor simulado");
           this.bloodGlucoseData = new Array(this.maxLen);
 	      this.endTidalCO2Data = new Array(this.maxLen);
 	      this.arrhythmiaIndex = new Array(this.maxLen);
 	  }else{
-		  console.log("Constructor sensor real");
-          // this.BatteryVoltage = new Array(this.maxLen);
-	      // this.BloodPressureDiastolitic = new Array(this.maxLen);
-	      // this.BloodPressureSystolic = new Array(this.maxLen);
-		  // this.HeartRate = new Array(this.maxLen);
-		  // this.Latitude = new Array(this.maxLen);
-		  // this.Longitude = new Array(this.maxLen);
-		  // this.MessageCounter = new Array(this.maxLen);
-		  // this.OxygenSaturation = new Array(this.maxLen);
-		  // this.PanicAlarm = new Array(this.maxLen);
-		  // this.RespiratoryRate = new Array(this.maxLen);
-		  // this.Satellites = new Array(this.maxLen);
+		  //console.log("Constructor sensor real");
+          this.BatteryVoltage = new Array(this.maxLen);
+	      this.BloodPressureDiastolitic = new Array(this.maxLen);
+	      this.BloodPressureSystolic = new Array(this.maxLen);
+		  this.HeartRate = new Array(this.maxLen);
+		  this.Latitude = new Array(this.maxLen);
+		  this.Longitude = new Array(this.maxLen);
+		  this.MessageCounter = new Array(this.maxLen);
+		  this.OxygenSaturation = new Array(this.maxLen);
+		  this.PanicAlarm = new Array(this.maxLen);
+		  this.RespiratoryRate = new Array(this.maxLen);
+		  this.Satellites = new Array(this.maxLen);
 		  this.Temperature = new Array(this.maxLen);
 	  }
     }
@@ -50,18 +50,33 @@ $(document).ready(() => {
       }
 	}
   
-	addRealData(time, temperature) {
+	addRealData(time, BatteryVoltage, BloodPressureDiastolitic, 
+				BloodPressureSystolic, HeartRate, OxygenSaturation, 
+				RespiratoryRate, Temperature) {
+					
       this.timeData.push(time);
-      this.Temperature.push(temperature);
+	  this.BatteryVoltage.push(BatteryVoltage);
+	  this.BloodPressureDiastolitic.push(BloodPressureDiastolitic);
+	  this.BloodPressureSystolic.push(BloodPressureSystolic);
+	  this.HeartRate.push(HeartRate);
+	  this.OxygenSaturation.push(OxygenSaturation);
+      this.RespiratoryRate.push(RespiratoryRate);
+	  this.Temperature.push(Temperature);
 
       if (this.timeData.length > this.maxLen) {
-        this.timeData.shift();
+		this.timeData.shift();
+        this.BatteryVoltage.shift();
+		this.BloodPressureDiastolitic.shift();
+		this.BloodPressureSystolic.shift();
+		this.HeartRate.shift();
+		this.OxygenSaturation.shift();
+		this.RespiratoryRate.shift();
         this.Temperature.shift();
       }
     }
 	
 	isDeviceSimulated(){
-		console.log("Valor de simulacion " + this.isSimulated);
+		//console.log("Valor de simulacion " + this.isSimulated);
 		return this.isSimulated;
 	}
 	
@@ -77,12 +92,12 @@ $(document).ready(() => {
     findDevice(deviceId) {
       for (let i = 0; i < this.devices.length; ++i) {
         if (this.devices[i].deviceId === deviceId) {
-		   console.log("Device Id encontrado:" + deviceId);
+		   //console.log("Device Id encontrado:" + deviceId);
           return this.devices[i];
         }
       }
 	
-	  console.log("Device Id no encontrado:" + deviceId);
+	  //console.log("Device Id no encontrado:" + deviceId);
       return undefined;
     }
 
@@ -170,25 +185,145 @@ $(document).ready(() => {
     datasets: [
       {
         fill: false,
-        label: 'Temperature',
-        yAxisID: 'Temperature',
+        label: 'BatteryVoltage',
+        yAxisID: 'BatteryVoltage',
         borderColor: 'rgba(255, 204, 0, 1)',
         pointBoarderColor: 'rgba(255, 204, 0, 1)',
         backgroundColor: 'rgba(255, 204, 0, 0.4)',
         pointHoverBackgroundColor: 'rgba(255, 204, 0, 1)',
         pointHoverBorderColor: 'rgba(255, 204, 0, 1)',
         spanGaps: true,
-      }]
+      },
+	  {
+      fill: false,
+      label: 'BloodPressureDiastolitic',
+      yAxisID: 'BloodPressureDiastolitic',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      pointBorderColor: 'rgba(255, 99, 132, 1)',
+      backgroundColor: 'rgba(255, 99, 132, 0.4)',
+      pointHoverBackgroundColor: 'rgba(255, 99, 132, 1)',
+      pointHoverBorderColor: 'rgba(255, 99, 132, 1)',
+      spanGaps: true,
+    },
+    {
+      fill: false,
+      label: 'BloodPressureSystolic',
+      yAxisID: 'BloodPressureSystolic',
+      borderColor: 'rgba(54, 162, 235, 1)',
+      pointBorderColor: 'rgba(54, 162, 235, 1)',
+      backgroundColor: 'rgba(54, 162, 235, 0.4)',
+      pointHoverBackgroundColor: 'rgba(54, 162, 235, 1)',
+      pointHoverBorderColor: 'rgba(54, 162, 235, 1)',
+      spanGaps: true,
+    },
+    {
+      fill: false,
+      label: 'HeartRate',
+      yAxisID: 'HeartRate',
+      borderColor: 'rgba(75, 192, 192, 1)',
+      pointBorderColor: 'rgba(75, 192, 192, 1)',
+      backgroundColor: 'rgba(75, 192, 192, 0.4)',
+      pointHoverBackgroundColor: 'rgba(75, 192, 192, 1)',
+      pointHoverBorderColor: 'rgba(75, 192, 192, 1)',
+      spanGaps: true,
+    },
+    {
+      fill: false,
+      label: 'OxygenSaturation',
+      yAxisID: 'OxygenSaturation',
+      borderColor: 'rgba(153, 102, 255, 1)',
+      pointBorderColor: 'rgba(153, 102, 255, 1)',
+      backgroundColor: 'rgba(153, 102, 255, 0.4)',
+      pointHoverBackgroundColor: 'rgba(153, 102, 255, 1)',
+      pointHoverBorderColor: 'rgba(153, 102, 255, 1)',
+      spanGaps: true,
+    },
+    {
+      fill: false,
+      label: 'RespiratoryRate',
+      yAxisID: 'RespiratoryRate',
+      borderColor: 'rgba(255, 159, 64, 1)',
+      pointBorderColor: 'rgba(255, 159, 64, 1)',
+      backgroundColor: 'rgba(255, 159, 64, 0.4)',
+      pointHoverBackgroundColor: 'rgba(255, 159, 64, 1)',
+      pointHoverBorderColor: 'rgba(255, 159, 64, 1)',
+      spanGaps: true,
+    },
+	{
+      fill: false,
+      label: 'Temperature',
+      yAxisID: 'Temperature',
+      borderColor: 'rgba(0, 200, 83, 1)',
+      pointBorderColor: 'rgba(0, 200, 83, 1)',
+      backgroundColor: 'rgba(0, 200, 83, 0.4)',
+      pointHoverBackgroundColor: 'rgba(0, 200, 83, 1)',
+      pointHoverBorderColor: 'rgba(0, 200, 83, 1)',
+      spanGaps: true,
+	}]
    };
    
    //chart options for simulated sensors
    const chartRealOptions = {
     scales: {
       yAxes: [{
+        id: 'BatteryVoltage',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'V',
+          display: true,
+        },
+        position: 'left',
+      },
+	  {
+        id: 'BloodPressureDiastolitic',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'ver',
+          display: true,
+        },
+        position: 'left',
+      },
+	  {
+        id: 'BloodPressureSystolic',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'Ver',
+          display: true,
+        },
+        position: 'left',
+      },
+	  {
+        id: 'HeartRate',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'ver',
+          display: true,
+        },
+        position: 'left',
+      },
+	  {
+        id: 'OxygenSaturation',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'ver',
+          display: true,
+        },
+        position: 'left',
+      },
+	  {
+        id: 'RespiratoryRate',
+        type: 'linear',
+        scaleLabel: {
+          labelString: 'ver',
+          display: true,
+        },
+        position: 'left',
+      },
+	  {
         id: 'Temperature',
         type: 'linear',
         scaleLabel: {
-          labelString: 'ºC',
+          labelString: 'ver',
           display: true,
         },
         position: 'left',
@@ -233,7 +368,13 @@ $(document).ready(() => {
         myLineChart.update();
 	}else{
 		chartRealData.labels = device.timeData;
-        chartRealData.datasets[0].data = device.Temperature || [];
+		chartRealData.datasets[0].data = device.BatteryVoltage || [];
+		chartRealData.datasets[1].data = device.BloodPressureDiastolitic || [];
+		chartRealData.datasets[2].data = device.BloodPressureSystolic || [];
+		chartRealData.datasets[3].data = device.HeartRate || [];
+		chartRealData.datasets[4].data = device.OxygenSaturation || [];
+		chartRealData.datasets[5].data = device.RespiratoryRate || [];
+        chartRealData.datasets[6].data = device.Temperature || [];
         myLineRealChart.update();
 	}
   }
@@ -263,7 +404,7 @@ $(document).ready(() => {
 			existingDeviceData.addData(messageData.MessageDate, messageData.IotData.bloodGlucose, messageData.IotData.endTidalCO2, messageData.IotData.arrhythmiaIndex );
 			console.log("Adding new data for simulated sensor");
 		}else{
-			existingDeviceData.addRealData(messageData.MessageDate, messageData.IotData.Temperature);
+			existingDeviceData.addRealData(messageData.MessageDate, messageData.IotData.BatteryVoltage, messageData.IotData.BloodPressureDiastolitic, messageData.IotData.BloodPressureSystolic, messageData.IotData.HeartRate, messageData.IotData.OxygenSaturation, messageData.IotData.RespiratoryRate ,messageData.IotData.Temperature);
 			console.log("Adding new data for real sensor");
 		}
       } else {
@@ -277,7 +418,7 @@ $(document).ready(() => {
 			newDeviceData.addData(messageData.MessageDate, messageData.IotData.bloodGlucose, messageData.IotData.endTidalCO2, messageData.IotData.arrhythmiaIndex);
 			console.log("First time adding new data for simulated sensor: " + messageData.IotData.bloodGlucose);
 		}else{
-			newDeviceData.addRealData(messageData.MessageDate, messageData.IotData.Temperature);
+			newDeviceData.addRealData(messageData.MessageDate, messageData.IotData.BatteryVoltage, messageData.IotData.BloodPressureDiastolitic, messageData.IotData.BloodPressureSystolic, messageData.IotData.HeartRate, messageData.IotData.OxygenSaturation, messageData.IotData.RespiratoryRate ,messageData.IotData.Temperature);
 			console.log("First time adding new data for real sensor: " + messageData.IotData.Temperature);
 		}
 
